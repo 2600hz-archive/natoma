e95916f499e65b0c7a5638cbbc0c0c442c620a38
Steve Lum <steve.lum@gmail.com>
2012-10-31 16:34:59 -0700

    testing the output of the number/account search

--- controllers/support.rb
+++ controllers/support.rb
@@ -1,21 +1,19 @@
 class Central
 	get '/support' do
 		@crumbs = []
-    	@crumbs << Central.crumb("Dashboard", "/")
-    	@active = Central.crumb("Support", "/support")
-
+    @crumbs << Central.crumb("Dashboard", "/")
+    @active = Central.crumb("Support", "/support")
 		haml "support/list"
 	end
 
 	get '/account_info' do
-		@foo
-		haml 'support/account_info'
+		haml 'support/account_info', :locals => { :session => session } 
 	end
 
 	post '/numbersearch' do
-		@foo = Numsearch.new(params[:number])
+		@foo = Numsearch.new.lookup(params[:number])
+		session[:foo1] = @foo
+		@foo.each {|k,v| puts "#{k} **************************  #{v}"}
 		redirect to('/account_info')
-		puts @foo
 	end
-
 end
