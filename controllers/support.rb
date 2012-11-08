a6fb3096e2831a032280ab3f237ac3c17fd7f4c7
Jeremy Ai <jeremy.ai@2600hz.com>
2012-11-08 10:46:34 -0800

    OPS-1004: Reverse print parent and ticket info
    direct parent first, etc
    most recent tickets first, etc

--- controllers/support.rb
+++ controllers/support.rb
@@ -11,13 +11,13 @@ class Central
 	end
 
 	post '/numbersearch' do
-		@foo, @bar, @meow, @mix, @zen = Numsearch.new.lookup(params[:number])
+		@raccount, @rparent, @rsugar, @rsugarcontact, @rticket = Numsearch.new.lookup(params[:number])
 		#session[:foo1] = @foo
 		#@foo.each {|k,v| puts "KEY:#{k}\nVALUE:#{v}\n\n\n"}
 		#@bar.each{|k,v| puts "KEY:#{k}\nVALUE:#{v}\n\n\n"}
 		#@meow.each{|k,v| puts "KEY:#{k}\nVALUE:#{v}\n\n\n"}
 		#@mix.each{|k,v| puts "KEY:#{k}\nVALUE:#{v}\n\n\n"}
 		#redirect to('/account_info')
-		haml 'support/account_info', :locals => { :foo => @foo , :bar => @bar , :meow => @meow , :mix => @mix, :zen => @zen}
+		haml 'support/account_info', :locals => { :raccount => @raccount , :rparent => @rparent , :rsugar => @rsugar , :rsugarcontact => @rsugarcontact , :rticket => @rticket}
 	end
 end
