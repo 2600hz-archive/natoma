47f4e52c11d746c536cacd49e8218d0534af01fd
Steve Lum <steve.lum@gmail.com>
2012-11-29 17:20:46 -0800

    added new routes for editing nodes

--- controllers/nodes.rb
+++ controllers/nodes.rb
@@ -28,6 +28,17 @@ class Central
     @logs = Log.new n_id
     haml "nodes/show"
   end
+
+  get '/nodes/edit/:node' do |n_id|
+    @node = Node.info(n_id)
+    @z_version = Central.redis.get "zones::#{@node["zone_id"]}::version"
+    @crumbs = []
+    @crumbs << Central.crumb("Dashboard", "/")
+    @crumbs << Central.crumb( "Nodes", "/nodes")
+    @active = Central.crumb(@node["name"] + " node", request.path_info)
+    @logs = Log.new n_id
+    haml "nodes/edit"
+  end
   
   post '/nodes' do
     id = counter
@@ -50,6 +61,13 @@ class Central
     @z = Zone.info(zone_id)
     n = Node.new(id)
     n.test(params, @z["erlang_cookie"])
+    redirect to("/nodes/#{node_id}")
+  end
+
+  post '/node_edit' do
+    node_id = params["node_id"]
+    ip = params["ip"]
+    Central.redis.hmset "nodes::#{node_id}", "name", params["name"], "ip", params["ip"]
     redirect to("/nodes/#{node_id}")
   end
 end
